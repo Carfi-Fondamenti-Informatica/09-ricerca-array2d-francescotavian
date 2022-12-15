fa15db48d00a8a2dfdd125e6b2bb2bb78e42b4f0
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-12-15 23:32:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
+    char lista[10][20];
+    char nome[10];
+    compila(lista);
+    compila2(nome);
+    confronto(lista,nome);
+    finale(lista,nome);
 
-int main(){
-  return 0;
+    return 0;
 }

726db3341ef310e367cc4aeb859d11bd97f1c309
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-12-15 23:31:59 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,49 @@
 
+using namespace std;
+#include <iostream>
+#include "lib.h"
+int z =0;
+void compila (char nomi[10][20]){
+    for (int i= 0; i < 10; i++) {
+        for (int k = 0; k < 20; k++) {
+            nomi[i][k] = ' ';
+        }
+    }
+    for (int i= 0; i < 10; i++) {
+        cin>>nomi[i];
+    }
+}
+void compila2(char nomi[]){
+    for (int k = 0; k < 20; k++) {
+        nomi[k]=' ';
+    }
+        cin>>nomi;
+
+    }
+
+
+bool confronto(char nomi[][20], char nomi2[]){
+    for (z=0;z<10;z++){
+
+        for (int k = 0; k < 20; k++) {
+            if (nomi[z][k] != nomi2[k])
+            {
+                break;
+            }
+            else if (k==19)
+            {
+                return true;
+            }
+        }
+    } return false;
+}
+void finale(char nomi[10][20], char nomi2[20]){
+    if (confronto(nomi, nomi2))
+    {
+        cout << z;
+    }
+    else
+    {
+        cout << "non presente";
+    }
+}

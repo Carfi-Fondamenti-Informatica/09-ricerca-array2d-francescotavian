da112d5ac28c7b19e3dd74bbe1303d437a063362
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-12-15 23:32:35 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,8 @@
 
+#ifndef ESERCITAZIONE_9_LIB_H
+#define ESERCITAZIONE_9_LIB_H
+void compila (char [10][20]);
+void compila2(char [20]);
+bool confronto(char [10][20], char [10]);
+void finale(char nomi[10][20], char nomi2[20]);
+#endif //ESERCITAZIONE_9_LIB_H

4452469d0bccb7763c3082314ee171c65a72cdb7
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-12-16 09:20:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,26 +2,26 @@ using namespace std;
 #include <iostream>
 #include "lib.h"
 int z=0;
-void compila(char nomi[10][20]){
+void compila(char a[10][20]){
     for (int i=0; i<10; i++{
         for(int k=0;k<20;k++){
-            nomi[i][k]=' ';
+            a[i][k]=' ';
         }
     }
          for (int i=0; i<10;i++){
-             cin>>nomi[i];
+             cin>>a[i];
          }
          }
- void compiladue(char nomi[]){
+ void compiladue(char a[]){
      for (int k=0;k<20;k++){
-         nomi[k]=' ';
+         a[k]=' ';
      }
-     cin>>nomi;
+     cin>>a;
  }
-       bool confronto(char nomi[][20],char n[]){
+       bool confronto(char a[][20],char n[]){
            for (z=0;z<10;z++){
                for (int k=0;k<20;k++){
-                   if (nomi[z][k] != n[k])
+                   if (a[z][k] != n[k])
                    {
                        break;
                    }
@@ -32,8 +32,8 @@ void compila(char nomi[10][20]){
                }
            }return false
            }
-         void finale (char nomi[10][20], char n[20]){
-             if (confronto(nomi,n))
+         void finale (char a[10][20], char n[20]){
+             if (confronto(a,n))
              {
                  cout<<z;
              }

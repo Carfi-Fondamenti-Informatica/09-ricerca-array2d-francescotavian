e5a66ade73f7317087a018193423241d069bb58d
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-12-16 09:15:12 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,45 +1,44 @@
 using namespace std;
 #include <iostream>
 #include "lib.h"
-int z = 0;
-void compila (char nomi[10][20]){
-    for (int i= 0; i < 10; i++) {
-        for (int k = 0; k < 20; k++) {
-            nomi[i][k] = ' ';
+int z=0;
+void compila(char nomi[10][20]){
+    for (int i=0; i<10; i++{
+        for(int k=0;k<20;k++){
+            nomi[i][k]=' ';
         }
     }
-    for (int i= 0; i < 10; i++) {
-        cin>>nomi[i];
-    }
-}
-void compila2(char nomi[]){
-    for (int k = 0; k < 20; k++) {
-        nomi[k]=' ';
-    }
-        cin>>nomi;
-    }
-bool confronto(char nomi[][20], char n[]){
-    for (z=0;z<10;z++){
-
-        for (int k = 0; k < 20; k++) {
-            if (nomi[z][k] != n[k])
-            
-                break;
-            
-            else if (k==19)
-            
-                return true;
-            
-        }
-    } return false;
-}
-void finale(char nomi[10][20], char n[20]){
-    if (confronto(nomi, n))
-    {
-        cout << z;
-    }
-    else
-    {
-        cout << "non presente";
-    }
-}
+         for (int i=0; i<10;i++){
+             cin>>nomi[i];
+         }
+         }
+ void compila2(char nomi[]){
+     for (int k=0;k<20;k++){
+         nomi[k]=' ';
+     }
+     cin>>nomi;
+ }
+       bool confronto(char nomi[][20],char n[]){
+           for (z=0;z<10;z++){
+               for (int k=0;k<20;k++){
+                   if (nomi[z][k] != n[k])
+                   {
+                       break;
+                   }
+                   else if (k==19)
+                   {
+                       return true;
+                   }
+               }
+           }return false
+           }
+         void finale (char nomi[10][20], char n[20]){
+             if (confronto(nomi,n))
+             {
+                 cout<<z;
+             }
+             else
+             {
+                 cout<<"non presente";
+             }
+         }
